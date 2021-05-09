68bf354216ae2014c2662a07906b9b44d95991c7
Andrey <a.boyko@stoloto.ru>
2021-05-09 23:05:19 +0300

    Задание №7: Сделать все тесты создания, удаления, модификации для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,6 +6,25 @@ class ContactHelper:
     def add(self, contact):
         wd = self.app.wd
         wd.find_element_by_xpath("//*[@id='nav']//a[text()='add new']").click()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        self.filing_in_the_fields(contact)
         wd.find_element_by_name("submit").click()
+
+    def filing_in_the_fields(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+
+    def delete_first(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//*[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
+    def modify_first(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//*[@title='Edit']").click()
+        self.filing_in_the_fields(contact)
+        wd.find_element_by_name("update").click()
--- fixture/group.py
+++ fixture/group.py
@@ -7,25 +7,35 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.filing_in_the_fields(group)
         wd.find_element_by_name("submit").click()
         self.open_groups_page()
+
+    def filing_in_the_fields(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
 
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def delete_first_group(self):
+    def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
+        self.open_groups_page()
+
+    def modify_first(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        self.filing_in_the_fields(group)
+        wd.find_element_by_name("update").click()
         self.open_groups_page()
