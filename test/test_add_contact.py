be49736e27bb18320c71ac0256ac9d4b6ad3eba6
Andrey <a.boyko@stoloto.ru>
2021-06-03 20:43:22 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,8 @@
-import random
-import string
-
-import pytest
-
 from model.contact import Contact
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(first_name=first_name, last_name=last_name)
-    for first_name in ["", random_string("first_name", 10)]
-    for last_name in ["", random_string("last_name", 10)]
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count()
