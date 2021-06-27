c77aa0589fdd998080d4b5f4b63246b57dc279af
Andrey <a.boyko@stoloto.ru>
2021-06-27 20:49:50 +0300

    test

--- fixture/Application.py
+++ fixture/Application.py
@@ -10,7 +10,7 @@ from fixture.session import SessionHelper
 class Application:
     def __init__(self, browser, base_url):
         if browser == "firefox":
-            self.wd = webdriver.Firefox(executable_path="../geckodriver")
+            self.wd = webdriver.Firefox(executable_path="C://geckodriver")
         elif browser == "chrome":
             self.wd = webdriver.Chrome()
         elif browser == "ie":
